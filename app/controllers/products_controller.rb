8768583422123c4cb635b9f39e7c9a42dcf5e35f
Julia Kruslin <julia.kruslin@gmail.com>
2020-11-30 18:08:04 +0100

    added products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,45 @@
+class ProductsController < ApplicationController
+skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product = Product.update(product_params)
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render "new" # create new form
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :descripton, :price)
+  end
+end
