e1e14327e54765dbbc76c971b05734dc56ab553c
Mees Barthen <mees@barthen.com>
2020-12-01 16:01:06 +0100

    changes to the map, stil not final

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,16 +5,21 @@ class ProductsController < ApplicationController
     @products = Product.all
     @user = User.all.where(seller_approved: true)
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-    @markers = @user.geocoded.map do |user|
-      {
-        lat: user.latitude,
-        lng: user.longitude
-      }
-    end
+    map
   end
 
   def show
     @product = Product.find(params[:id])
+    # @user = @product.user
+    @user = User.all.where(seller_approved: true)
+    map
+    # @markers = @user.geocode.map do |user| {
+    #   lat: user.latitude,
+    #   lng: user.longitude,
+    #   infoWindow: render_to_string(partial: "info_window", locals: { user: user }),
+    #   image_url: helpers.asset_url('shop_marker')
+    # }
+    # end
   end
 
   def edit
@@ -32,7 +37,7 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-# create is not working yet
+  # create is not working yet
 
   def create
     @product = Product.new(product_params)
@@ -49,9 +54,27 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def map
+    @markers = @user.geocoded.map do |user| {
+      lat: user.latitude,
+      lng: user.longitude,
+      infoWindow: render_to_string(partial: "info_window", locals: { user: user }),
+      image_url: helpers.asset_url('shop_marker')
+    }
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :price, photos: [])
   end
 end
+
+
+
+
+
+
+
+
