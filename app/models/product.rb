2a1796cfa7d8e0ab1dc583ce054605fd8ae20e76
Julia Kruslin <julia.kruslin@gmail.com>
2020-12-08 12:11:21 +0100

    searchbar on top fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,7 +18,7 @@ class Product < ApplicationRecord
 
  include PgSearch::Model
   pg_search_scope :global_search,
-    against: [:name, :description],
+    against: [:name, :description,],
     associated_against: {
       user: [:first_name, :last_name]
     },
